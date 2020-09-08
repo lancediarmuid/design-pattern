6c8f898c5c91bc60cd04f1916aaad41b4f220be6
lance <lvtao0626@163.com>
2020-09-08 23:43:22 +0800

    <chore>: 结合单例优化简单工厂模式

--- patterns/factorypattern/simplefactory/SimpleAttachmentFactory.java
+++ patterns/factorypattern/simplefactory/SimpleAttachmentFactory.java
@@ -3,25 +3,32 @@ package factorypattern.simplefactory;
 
 import factorypattern.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 简单工厂
  * 背景设定：
  *     附件的CURD，根据不同的业务类型分别创建不同的业务类型附件对象
  */
 public class SimpleAttachmentFactory {
+
+    private static final Map<String, IBusinessAttachment> cachedMap = new HashMap<>();
+
+    static {
+        cachedMap.put("WorkOrder", new WorkOrderAttachment());
+        cachedMap.put("AftersaleOrder", new AftersaleOrderAttachment());
+        cachedMap.put("CustomerServiceOrder", new CustomerServiceOrderAttachment());
+        cachedMap.put("RefundOrder", new RefundOrderAttachment());
+        cachedMap.put("BrandOrder", new BrandOrderAttachment());
+    }
+
     public static IBusinessAttachment create(String businessType){
-        IBusinessAttachment iBusinessAttachment = null;
-        if ("WorkOrder".equals(businessType)){
-            iBusinessAttachment = new WorkOrderAttachment();
-        }else if ("AftersaleOrder".equals(businessType)){
-            iBusinessAttachment = new AftersaleOrderAttachment();
-        }else if ("CustomerServiceOrder".equals(businessType)){
-            iBusinessAttachment = new CustomerServiceOrderAttachment();
-        }else if ("RefundOrder".equals(businessType)){
-            iBusinessAttachment = new RefundOrderAttachment();
-        }else if ("BrandOrder".equals(businessType)){
-            iBusinessAttachment = new BrandOrderAttachment();
+        if (businessType == null|| businessType.isEmpty()){
+            return null;
         }
+
+        IBusinessAttachment iBusinessAttachment = cachedMap.get(businessType);
 
         return iBusinessAttachment;
     }
